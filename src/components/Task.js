6c1624d0ba0fd598d024e4d8f7eae28b01040af4
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-11-20 00:56:41 +0300

    Delete button

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Task({text, category}) {
+function Task({text, category,onDelete}) {
   // console.log(text)
   return (
     <div className="task">
       <div className="label">CATEGORY HERE: {category}</div>
       <div className="text">TEXT HERE: {text}</div>
-      <button className="delete">X</button>
+      <button className="delete" onClick={()=>onDelete(text)}>X</button>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -5,11 +5,16 @@ import Task from "./Task";
 function TaskList() {
   const [tasks, setTasks]= useState(TASKS)
   // console.log(tasks)
+  function handleDelete(taskText) {
+    // Filter out the task with the given text
+    const updatedTasks = tasks.filter(task => task.text !== taskText);
+    setTasks(updatedTasks);
+  }
   return (
     <div>
       {tasks.map((task,index)=>{
           return(
-            <li key={index}><Task text= {task.text} category= {task.category}/></li>
+            <li key={index}><Task text= {task.text} category= {task.category} onDelete={handleDelete}/></li>
           )
         })}
       {/* display a list of tasks using Task component */}
